dd19f472802b777b1c5749b2bf9ada635152e13b
Lad-Rahul <rahullad945@gmail.com>
2023-04-23 23:42:12 +0530

    initial commit

---
+++ src/main/java/com/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,62 @@
+package com.rest.webservices.restfulwebservices.user;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+public class UserResource {
+
+	private UserDaoService service;
+
+	public UserResource(UserDaoService service) {
+		this.service = service;
+	}
+
+	// GET /users
+	@GetMapping("/users")
+	public List<User> retriveAllUsers() {
+		return service.findAllUsers();
+	}
+
+	// GET /users/{id}
+	@GetMapping("/users/{id}")
+	public User retriveUser(@PathVariable int id) {
+		User user = service.findOne(id);
+		
+		if(user == null) {
+			throw new UserNotFoundException("id : " + id);
+		}
+		
+		return user;
+	}
+
+	// POST /users
+	@PostMapping("/users")
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+		User newUser = service.save(user);
+		URI location = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(newUser.getId())
+				.toUri();
+		
+		
+		return ResponseEntity.created(location).build();
+	}
+	
+	//DELETE /usres/{id}
+	@DeleteMapping("/users/{id}")
+	public void deleteUser(@PathVariable int id) {
+		service.deleteById(id);
+	}
+
+}
